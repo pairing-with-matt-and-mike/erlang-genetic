5ab32c79e0857d35cd7d73f3c60bf19004ee0113
Michael Williamson <mike@zwobble.org>
2013-10-20 22:05:38 +0100

    Add an alternative strategy that uses mutation

--- genetic.erl
+++ genetic.erl
@@ -49,9 +49,56 @@ evaluate({Operator, First, Second}, BoundInputs) ->
 
 generate_best_function(Training, Vars) ->
     Generation = map(fun(_) -> generate_node(Vars) end, lists:seq(1, 5000)),
+    By_fitness = generation_by_fitness(Training, Vars, Generation),
+    lists:sublist(By_fitness, 5).
+
+generation_by_fitness(Training, Vars, Generation) ->
     Results = map(fun(G) -> {G, calculate_fitness(Training, Vars, G)} end, Generation),
-    lists:sublist(lists:sort(fun ({_, A}, {_, B}) -> A > B end, Results), 5).
+    lists:sort(fun ({_, A}, {_, B}) -> A > B end, Results).
+
 
 generate_average_function() ->
     Training = generate_averages(),
     generate_best_function(Training, [a, b]).
+
+
+mutate_function({number, _}, Vars) -> generate_node(Vars);
+mutate_function({var, _}, Vars) -> generate_node(Vars);
+mutate_function({Operator, Left, Right}, Vars) ->
+    New_node = generate_node(Vars),
+    case uniform(5) of
+         1 -> {Operator, New_node, Right};
+         2 -> {Operator, Left, New_node};
+         3 -> Left;
+         4 -> Right;
+         _ -> New_node
+    end.
+
+generate_average_function_mutation(GenerateGeneration) ->
+    Training = generate_averages(),
+    Vars = [a, b],
+    Initial = generate_node(Vars),
+    lists:foldl(
+        fun(_, Parent) ->
+           Generation = GenerateGeneration(Parent, Vars),
+           [{Winner, _}|_] = generation_by_fitness(Training, Vars, Generation),
+           Winner
+        end,
+        Initial,
+        lists:seq(1, 1000)
+    ).
+
+
+generate_mutant_generation(Depth, Parent, Vars) ->
+    lists:foldl(
+        fun(_, Ancestors) ->
+            [mutate_function(hd(Ancestors), Vars)|Ancestors]
+        end,
+        [Parent],
+        lists:seq(1, Depth)
+    ).
+
+go(Depth) ->
+    generate_average_function_mutation(fun(Parent, Vars) ->
+        generate_mutant_generation(Depth, Parent, Vars)
+    end).
